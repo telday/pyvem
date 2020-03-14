3243a2ef2ee9c60b6412e5c905cd92984081db78
telday <ejw393@gmail.com>
2020-03-13 21:17:12 -0400

    Finished environment class

--- pyvem/environment.py
+++ pyvem/environment.py
@@ -1,16 +1,16 @@
 
-class Environment:
-    VALID_KWARGS = ['system_site_packages', 'clear', 'symlinks', 'upgrade', 'with_pip','installed_packages', 'python_version']
-    REQUIRED_KWARGS = ['prompt',  'location']
+VALID_KWARGS = ['system_site_packages', 'clear', 'symlinks', 'upgrade', 'with_pip','installed_packages', 'python_version']
+REQUIRED_KWARGS = ['prompt',  'location']
 
+class Environment:
     def __init__(self, **kwargs):
         for i in REQUIRED_KWARGS:
             if i not in kwargs.keys():
                 raise AttributeError("Environment requires args",
-                        "{}".format(self.REQUIRED_ARGS))
+                        "{}".format(REQUIRED_KWARGS))
 
         for i in kwargs:
-            if i not in self.VALID_KWARGS + self.REQUIRED_KWARGS:
+            if i not in VALID_KWARGS + REQUIRED_KWARGS:
                 raise AttributeError(
                             "{} is not a valid attribute for Environment"
                         )

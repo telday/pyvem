a590a47271c818325486987fe4a3368f83fd20ac
telday <ejw393@gmail.com>
2020-03-25 21:40:29 -0400

    Finished environment class and tests

--- pyvem/environment.py
+++ pyvem/environment.py
@@ -1,18 +1,20 @@
 
-VALID_KWARGS = ['system_site_packages', 'clear', 'symlinks', 'upgrade', 'with_pip','installed_packages', 'python_version']
-REQUIRED_KWARGS = ['prompt',  'location']
 
 class Environment:
     """Represents details about an environment, just acts as a setter/getter"""
+
+    VALID_KWARGS = ['system_site_packages', 'clear', 'symlinks', 'upgrade', 'with_pip','installed_packages', 'python_version']
+    REQUIRED_KWARGS = ['prompt',  'location']
+
     def __init__(self, **kwargs):
         """Gives every kwarg an attribute"""
-        for i in REQUIRED_KWARGS:
+        for i in self.REQUIRED_KWARGS:
             if i not in kwargs.keys():
                 raise AttributeError("Environment requires args",
-                        "{}".format(REQUIRED_KWARGS))
+                        "{}".format(self.REQUIRED_KWARGS))
 
         for i in kwargs:
-            if i not in VALID_KWARGS + REQUIRED_KWARGS:
+            if i not in self.VALID_KWARGS + self.REQUIRED_KWARGS:
                 raise AttributeError(
                             "{} is not a valid attribute for Environment"
                         )
--- tests/test_environment.py
+++ tests/test_environment.py
@@ -1,30 +1,32 @@
-
+import itertools
 import unittest
-
+import pyvem.environment as e
 
 class TestEnvironment(unittest.TestCase):
-
-
-    def powerset(iterable):
+    """Tests the environment class"""
+    def powerset(self, iterable):
+        """Gets the powerset of the iterable"""
         s = list(iterable)
-        return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
+        return list(itertools.chain.from_iterable(itertools.combinations(s, r) for r in range(len(s)+1)))
+
+    def check_has_required(self, args):
+        """Checks if the required kwargs are listed in the list args"""
+        for i in e.Environment.REQUIRED_KWARGS:
+            if i not in args:
+                return False
+        return True
 
     def test_constructor(self):
-        KWARGS = {
-                'system_site_packages':1,
-                'clear':2,
-                'symlinks':3,
-                'upgrade':4,
-                'with_pip':5,
-                'installed_packages':6,
-                'python_version':7,
-            }
-
-        REQUIRED_KWARGS = {
-                'prompt':8,
-                'location':9,
-            }
-
-        combos = powerset(KWARGS + REQUIRED_KWARGS)
-        for i in KWARGS:
-            if 
+        """Tests the setup of getters and setters for the Environment class"""
+        combos = self.powerset(e.Environment.VALID_KWARGS + e.Environment.REQUIRED_KWARGS)
+        for i in range(len(combos)):
+            kwargs = dict()
+            for j in range(len(combos[i])):
+                kwargs[combos[i][j]] = i
+            if self.check_has_required(list(kwargs.keys())):
+                tmp_env = e.Environment(**kwargs)
+                for j in range(len(combos[i])):
+                    self.assertEqual(tmp_env.__getattribute__(combos[i][j]), i)
+            else:
+                with self.assertRaises(Exception):
+                    tmp_env = e.Environment(**kwargs)
